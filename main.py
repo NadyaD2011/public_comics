cb535eb08d0616bcbaf14fb05121366182c33d9b
NadyaEvp <t_evp@mail.ru>
2024-08-10 21:26:26 +0300

    Все числа связаны с кодом

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import shutil
 from dotenv import load_dotenv
 
 
-def get_comics(random_number):
-    comics_url = f'https://xkcd.com/{random_number}/info.0.json'
+def get_comics(random_number_comic):
+    comics_url = f'https://xkcd.com/{random_number_comic}/info.0.json'
     response = requests.get(comics_url)
     response.raise_for_status()
     comics_url = response.json()['img']
@@ -15,15 +15,15 @@ def get_comics(random_number):
     return comics_url, comics_comment
 
 
-def save_comics(comics_url, folder_name, random_number):
+def save_comics(comics_url, folder_name, random_number_comic):
     comics = requests.get(comics_url)
     comics.raise_for_status()
-    with open(f'{folder_name}/comics{random_number}.png', 'wb') as file:
+    with open(f'{folder_name}/comics{random_number_comic}.png', 'wb') as file:
         file.write(comics.content)
 
 
-def publish_photos(bot, chat_id, folder_name, text_post, random_number):
-    with open(f'{folder_name}/comics{random_number}.png', 'rb') as document:
+def publish_photos(bot, chat_id, folder_name, text_post, random_number_comic):
+    with open(f'{folder_name}/comics{random_number_comic}.png', 'rb') as document:
         bot.send_document(chat_id=chat_id, document=document, caption=text_post)
 
 def main():
@@ -31,14 +31,14 @@ def main():
     folder_name = 'images'
     bot_token = os.environ['TELEGRAM_BOT_ID']
     chat_id = os.environ['CHAT_ID_TELEGRAM']
-    random_number = random.randint(1,2700)
+    random_number_comic = random.randint(1,2700)
     bot = telegram.Bot(bot_token)
     
     try:
         os.makedirs(folder_name, mode=0o777, exist_ok=True)
-        comics_url, text_post = get_comics(random_number)
-        save_comics(comics_url, folder_name, random_number)
-        publish_photos(bot, chat_id, folder_name, text_post, random_number)
+        comics_url, text_post = get_comics(random_number_comic)
+        save_comics(comics_url, folder_name, random_number_comic)
+        publish_photos(bot, chat_id, folder_name, text_post, random_number_comic)
     finally:
         shutil.rmtree(folder_name)
 
