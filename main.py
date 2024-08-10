4071f6c1f60f17145e03e3782645369a319e5775
NadyaEvp <t_evp@mail.ru>
2024-08-10 21:28:35 +0300

    Чиним множественное число

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import shutil
 from dotenv import load_dotenv
 
 
-def get_comics(random_number_comic):
+def get_comic(random_number_comic):
     comics_url = f'https://xkcd.com/{random_number_comic}/info.0.json'
     response = requests.get(comics_url)
     response.raise_for_status()
@@ -15,14 +15,14 @@ def get_comics(random_number_comic):
     return comics_url, comics_comment
 
 
-def save_comics(comics_url, folder_name, random_number_comic):
+def save_comic(comics_url, folder_name, random_number_comic):
     comics = requests.get(comics_url)
     comics.raise_for_status()
     with open(f'{folder_name}/comics{random_number_comic}.png', 'wb') as file:
         file.write(comics.content)
 
 
-def publish_photos(bot, chat_id, folder_name, text_post, random_number_comic):
+def publish_comic(bot, chat_id, folder_name, text_post, random_number_comic):
     with open(f'{folder_name}/comics{random_number_comic}.png', 'rb') as document:
         bot.send_document(chat_id=chat_id, document=document, caption=text_post)
 
@@ -36,9 +36,9 @@ def main():
     
     try:
         os.makedirs(folder_name, mode=0o777, exist_ok=True)
-        comics_url, text_post = get_comics(random_number_comic)
-        save_comics(comics_url, folder_name, random_number_comic)
-        publish_photos(bot, chat_id, folder_name, text_post, random_number_comic)
+        comics_url, text_post = get_comic(random_number_comic)
+        save_comic(comics_url, folder_name, random_number_comic)
+        publish_comic(bot, chat_id, folder_name, text_post, random_number_comic)
     finally:
         shutil.rmtree(folder_name)
 
